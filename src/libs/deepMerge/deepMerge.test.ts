d977dbcc8f508f41f9c92589f1921de803121863
Alexis Mineaud <amineaud@tenable.com>
2023-04-07 18:37:26 +0200

    Extends deepMerge behaviors.
    - Allow merging until 6 objects
    - Do not mutate passed objects

--- src/libs/deepMerge/deepMerge.test.ts
+++ src/libs/deepMerge/deepMerge.test.ts
@@ -1,73 +1,154 @@
 import { deepMerge } from '.'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, expectTypeOf, it } from 'vitest'
 
 describe('deepMerge', () => {
-  it('should deep merge two objects', () => {
-    const o1 = {
-      '1': '1',
-      '2': {
-        '2.1': '2.1'
-      }
-    }
-
-    const o2 = {
-      '1': '1',
-      '2': {
-        '2.2': '2.2'
-      },
-      '3': {
-        '3.1': {
-          '3.3.1': '3.3.1'
+  describe('Merging', () => {
+    it('should deep merge two objects', () => {
+      const o1 = {
+        '1': '1',
+        '2': {
+          '2.1': '2.1'
         }
       }
-    }
 
-    expect(deepMerge([o1, o2])).toEqual({
-      '1': '1',
-      '2': { '2.1': '2.1', '2.2': '2.2' },
-      '3': { '3.1': { '3.3.1': '3.3.1' } }
+      const o2 = {
+        '1': '1',
+        '2': {
+          '2.2': '2.2'
+        },
+        '3': {
+          '3.1': {
+            '3.3.1': '3.3.1'
+          }
+        }
+      }
+
+      expect(deepMerge([o1, o2])).toEqual({
+        '1': '1',
+        '2': { '2.1': '2.1', '2.2': '2.2' },
+        '3': { '3.1': { '3.3.1': '3.3.1' } }
+      })
+    })
+
+    it('should deep merge n objects', () => {
+      const o1 = {
+        '1': '1'
+      }
+
+      const o2 = {
+        '2': '2'
+      }
+
+      const o3 = {
+        '3': '3'
+      }
+
+      const o4 = {
+        '4': '4'
+      }
+
+      const mergedObjects = deepMerge([o1, o2, o3, o4])
+
+      expect(mergedObjects).toEqual({
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4'
+      })
+
+      type MergedObjectType = typeof o1 & typeof o2 & typeof o3 & typeof o4
+
+      expectTypeOf(mergedObjects).toMatchTypeOf<MergedObjectType>()
+    })
+
+    it('should concat arrays by default', () => {
+      const o1 = {
+        '1': '1',
+        '2': {
+          '2.1': ['two']
+        }
+      }
+
+      const o2 = {
+        '1': '1',
+        '2': {
+          '2.1': ['dot', 'one']
+        }
+      }
+
+      expect(deepMerge([o1, o2])).toEqual({
+        '1': '1',
+        '2': { '2.1': ['two', 'dot', 'one'] }
+      })
     })
   })
 
-  it('should concat arrays by default', () => {
-    const o1 = {
-      '1': '1',
-      '2': {
-        '2.1': ['two']
-      }
-    }
+  describe('Options', () => {
+    describe('concatArray', () => {
+      it('should not concat arrays when using `concatArray:false`', () => {
+        const o1 = {
+          '1': '1',
+          '2': {
+            '2.1': ['two']
+          }
+        }
 
-    const o2 = {
-      '1': '1',
-      '2': {
-        '2.1': ['dot', 'one']
-      }
-    }
+        const o2 = {
+          '1': '1',
+          '2': {
+            '2.1': ['dot', 'one']
+          }
+        }
 
-    expect(deepMerge([o1, o2])).toEqual({
-      '1': '1',
-      '2': { '2.1': ['two', 'dot', 'one'] }
+        expect(deepMerge([o1, o2], { concatArray: false })).toEqual({
+          '1': '1',
+          '2': { '2.1': ['dot', 'one'] }
+        })
+      })
     })
   })
 
-  it('should not concat arrays when using `concatArray:false`', () => {
-    const o1 = {
-      '1': '1',
-      '2': {
-        '2.1': ['two']
+  describe('Mutation', () => {
+    it('should not mutate objects', () => {
+      const o1 = {
+        '1': '1',
+        '2': {
+          '2.1': '2.1'
+        }
       }
-    }
 
-    const o2 = {
-      '1': '1',
-      '2': {
-        '2.1': ['dot', 'one']
+      const o2 = {
+        '1': '1',
+        '2': {
+          '2.2': '2.2'
+        },
+        '3': {
+          '3.1': {
+            '3.3.1': '3.3.1'
+          }
+        }
       }
-    }
 
-    expect(deepMerge([o1, o2], { concatArray: false })).toEqual({
-      '1': '1',
-      '2': { '2.1': ['dot', 'one'] }
+      deepMerge([o1, o2])
+
+      expect(o1).toEqual({
+        '1': '1',
+        '2': {
+          '2.1': '2.1'
+        }
+      })
+
+      expect(o2).toEqual({
+        '1': '1',
+        '2': {
+          '2.2': '2.2'
+        },
+        '3': {
+          '3.1': {
+            '3.3.1': '3.3.1'
+          }
+        }
+      })
     })
   })
 })
