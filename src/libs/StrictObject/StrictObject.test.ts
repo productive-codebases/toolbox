d12980aa5ab3a06326de5bc634aae3f470a9420d
Alexis Mineaud <amineaud@tenable.com>
2023-10-16 13:46:58 +0200

    Extract types.

--- src/libs/StrictObject/StrictObject.test.ts
+++ src/libs/StrictObject/StrictObject.test.ts
@@ -8,16 +8,24 @@ describe('StrictObject', () => {
 
   it('returns keys', () => {
     const keys = StrictObject.keys(obj)
+
     expect(keys).toEqual(['foo', 'bar'])
   })
 
   it('returns values', () => {
+    const obj: Record<'fooKey' | 'barKey', 'foo' | 'bar'> = {
+      fooKey: 'foo',
+      barKey: 'bar'
+    }
+
     const values = StrictObject.values(obj)
-    expect(values).toEqual(['foo value', 'bar value'])
+
+    expect(values).toEqual(['foo', 'bar'])
   })
 
   it('returns entries', () => {
     const entries = StrictObject.entries(obj)
+
     expect(entries).toEqual([
       ['foo', 'foo value'],
       ['bar', 'bar value']
--- src/libs/StrictObject/index.ts
+++ src/libs/StrictObject/index.ts
@@ -1,16 +1,4 @@
-// From https://stackoverflow.com/a/69019874
-type ObjectType = Record<PropertyKey, unknown>
-
-type PickByValue<T, V> = Pick<
-  // From https://stackoverflow.com/a/55153000
-  T,
-  { [K in keyof T]: T[K] extends V ? K : never }[keyof T]
->
-
-type ObjectEntries<T> = {
-  // From https://stackoverflow.com/a/60142095
-  [K in keyof T]: [keyof PickByValue<T, T[K]>, T[K]]
-}[keyof T][]
+import { ObjectType, ObjectEntries } from './types'
 
 function strictKeys<T extends ObjectType>(source: T): Array<keyof T> {
   return Object.keys(source) as Array<keyof T>
